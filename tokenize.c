5e6173fe0ac6ecc88a7d9bba60c978fed846c7ab
Chin Woon Koh <chikoh@c1r2s5.42singapore.sg>
2025-08-16 22:10:36 +0800

    Ensure that the open brackets are compared in the string tokenization

--- tokenize.c
+++ tokenize.c
@@ -6,7 +6,7 @@
 /*   By: chikoh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 16:09:08 by chikoh            #+#    #+#             */
-/*   Updated: 2025/08/12 22:09:17 by chikoh           ###   ########.fr       */
+/*   Updated: 2025/08/16 22:06:46 by chikoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,7 +120,7 @@ t_list	*construct_string(char **string)
 	while ((**string != '\'') && (**string != '"') && (**string != '>')
 		&& (**string != '<') && (**string != '|') && (**string != '&')
 		&& (**string != ' ') && (**string != '$') && (**string != ')')
-		&& (**string != '=') && (**string != ')') && **string)
+		&& (**string != '=') && (**string != '(') && **string)
 		(*string)++;
 	result->string = (char *)ft_calloc(sizeof(char),
 			*string - string_start + 1);
