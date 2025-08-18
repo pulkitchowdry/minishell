6c047beb9b219f999209acbb960ad9313bd8bf24
Chin Woon Koh <chikoh@c1r2s4.42singapore.sg>
2025-08-18 15:45:55 +0800

    Added the initial forking of the execution of the command

--- execute.c
+++ execute.c
@@ -6,7 +6,7 @@
 /*   By: chikoh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 20:40:39 by chikoh            #+#    #+#             */
-/*   Updated: 2025/08/17 17:40:31 by chikoh           ###   ########.fr       */
+/*   Updated: 2025/08/17 22:21:10 by chikoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,101 @@ void	execute_heredoc(t_ast_node *node)
 	}
 	if (node->redirection != 0)
 		open_temp_files(node->redirection);
+}
+
+char	execute_logical_or(t_ast_node *node)
+{
+	int	pid;
+	int	ret_code;
+
+	pid = fork();
+	if (pid == 0)
+		execute_command(node->left);
+	else
+		wait_pid(pid, &ret_code, 0);
+	if (WIFEXITED(ret_code))
+	{
+		if (WEXITSTATUS(ret_code) == 0)
+			return (WEXITSTATUS(ret_code));
+		pid = fork();
+		if (pid == 0)
+			execute_command(node->right);
+		else
+			wait_pid(pid, &ret_code, 0);
+		if (WIFEXITED(ret_code))
+			return (WEXITSTATUS(ret_code));
+		else if (WIFSIGNALED(ret_code))
+			return (128 + WTERMSIG(ret_code));
+	}
+	else if (WIFSIGNALED(ret_code))
+		return (128 + WTERMSIG(ret_code));
+	return (255);
+}
+
+char	execute_logical_and(t_ast_node *node)
+{
+	int	pid;
+	int	ret_code;
+
+	pid = fork();
+	if (pid == 0)
+		execute_command(node->left);
+	else
+		wait_pid(pid, &ret_code, 0);
+	if (WIFEXITED(ret_code))
+	{
+		if (WEXITSTATUS(ret_code) != 0)
+			return (WEXITSTATUS(ret_code));
+		pid = fork();
+		if (pid == 0)
+			execute_command(node->right);
+		else
+			wait_pid(pid, &ret_code, 0);
+		if (WIFEXITED(ret_code))
+			return (WEXITSTATUS(ret_code));
+		else if (WIFSIGNALED(ret_code))
+			return (128 + WTERMSIG(ret_code));
+	}
+	else if (WIFSIGNALED(ret_code))
+		return (128 + WTERMSIG(ret_code));
+	return (255);
+}
+
+char	execute_pipe(t_ast_node *node)
+{
+	int	pid[2];
+	int	ret_code;
+	int	proc_count;
+
+	pid[0] = fork();
+	if (pid[0] == 0)
+		execute_command(node->left);
+	else
+		pid[1] = fork();
+	proc_count = 0;
+	while (proc_count < 2)
+	{
+		wait_pid(pid[proc_count], &ret_code, 0);
+		proc_count++;
+	}
+	if (WIFEXITED(ret_code))
+		return (WEXITSTATUS(ret_code));
+	else if (WIFSIGNALED(ret_code))
+		return (128 + WTERMSIG(ret_code));
+	return (255);
+}
+
+void	execute_command_ast(t_ast_node *node)
+{
+	if (node == 0)
+		return ;
+	if (node->node != 0)
+		execute_command(node);
+	else if (node->node->type == LOGICAL_OR)
+		execute_logical_or(node);
+	else if (node->node->type == LOGICAL_AND)
+		execute_logical_and(node);
+	else if (node->node->type == PIPE)
 }
 
 void	unlink_temp_files(t_list *list)
