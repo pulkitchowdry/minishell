b3e6b15ba3867a02cbd9c1e471b66f2e67c0fbf4
Pulkit Chowdry H <pulkitchowdry@gmail.com>
2025-08-29 14:43:48 +0800

    Fixed a bug in dup environment variables. Also fixed some memory leaks and reduced function sizes under 25. Still few more functions left and memory leak to be checked further.

--- built_in_helper.c
+++ built_in_helper.c
@@ -6,7 +6,7 @@
 /*   By: pchowdry <pchowdry@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2025/08/28 23:14:37 by pchowdry         ###   ########.fr       */
+/*   Updated: 2025/08/29 14:41:25 by pchowdry         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,99 +137,155 @@ char	**ft_dup_envp(char **envp)
 // 	return (copy);
 // }
 
+int	ft_find_equal(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i++;
+	return (i);
+}
+
+char	*ft_get_value(char *str)
+{
+	int		i;
+	char	*result;
+	int		equal_pos;
+	
+	i = 0;
+	equal_pos = ft_find_equal(str);
+	if (ft_strlen(str) - equal_pos == 0)
+		return (NULL);
+	result = ft_calloc(sizeof(char), ft_strlen(str) - equal_pos);
+	equal_pos++;
+	while (equal_pos < ft_strlen(str))
+	{
+		result[i] = str[equal_pos];
+		equal_pos++;
+		i++;
+	}
+	return(result);
+}
+
+char	*ft_get_key(char *str)
+{
+	int		i;
+	char	*result;
+	int		equal_pos;
+	
+	i = 0;
+	equal_pos = ft_find_equal(str);
+	if (equal_pos == 0)
+		return (NULL);
+	result = ft_calloc(sizeof(char), equal_pos + 1);
+	while (i < equal_pos)
+	{
+		result[i] = str[i];
+		i++;
+	}
+	return(result);
+}
+
+char	*join_dup_envp(char *key, char *value)
+{
+	char	*join;
+	
+	join = ft_strjoin(key, "=");
+	join = ft_strjoin(join, "\"");
+	join = ft_strjoin(join, value);
+	join = ft_strjoin(join, "\"");
+	return (join);
+}
+
 //Add values into myenvp also when its added in default envp
 void	ft_add_to_myenvp(t_variable_context *context, t_env *temp_env)
 {
 	int		i;
 	char	*temp_key;
+	char	*extract;
 
 	i = 0;
 	temp_key = ft_strjoin("declare -x ", temp_env->new_env_key);
 	while (context->dup_environment_variables[i])
 	{
-		if (ft_strncmp(context->dup_environment_variables[i], temp_key, ft_strlen(temp_key)) == 0)
+		extract = ft_get_key(context->dup_environment_variables[i]);
+		if (ft_strncmp(extract, temp_key, ft_strlen(extract) + 1) == 0)
 		{
 			if (temp_env->new_env_value)
-			{
-				context->dup_environment_variables[i] = ft_strjoin(temp_key, "=");
-				context->dup_environment_variables[i] = ft_strjoin(context->dup_environment_variables[i], "\"");
-				context->dup_environment_variables[i] = ft_strjoin(context->dup_environment_variables[i], temp_env->new_env_value);
-				context->dup_environment_variables[i] = ft_strjoin(context->dup_environment_variables[i], "\"");
-				return ;
-			}
+				context->dup_environment_variables[i] = join_dup_envp(temp_key, temp_env->new_env_value);
 			else
-			{
 				context->dup_environment_variables[i] = ft_strdup(temp_key);
-				return ;
-			}
+			return ;
 		}
 		i++;
 	}
 	if (temp_env->new_env_value)
-	{
-		temp_key = ft_strjoin(temp_key, "=");
-		temp_key = ft_strjoin(temp_key, "\"");
-		temp_key = ft_strjoin(temp_key, temp_env->new_env_value);
-		temp_key = ft_strjoin(temp_key, "\"");
-		context->dup_environment_variables = ft_copy_envp(context->dup_environment_variables, temp_key);
-	}
-	else
-		context->dup_environment_variables = ft_copy_envp(context->dup_environment_variables, temp_key);
+		temp_key = join_dup_envp(temp_key, temp_env->new_env_value);
+	context->dup_environment_variables = ft_copy_envp(context->dup_environment_variables, temp_key);
 }
 
-//To update the entries in envp
-void	ft_update_envp(t_env *temp_envp, t_list *command, t_variable_context *context)
+void	ft_update_envp_exp(t_env *temp_envp, t_list *command, t_variable_context *context)
 {
 	int		i;
 	char	*temp_key;
 
 	i = 0;
-	if (ft_strncmp(command->content, "cd", ft_strlen(command->content)) == 0)
+	temp_key = ft_strjoin(temp_envp->new_env_key, "=");
+	while (context->environment_variables[i])
 	{
-		while (context->environment_variables[i])
+		if (ft_strncmp(context->environment_variables[i], temp_key, ft_strlen(temp_key)) == 0)
 		{
-			if (ft_strncmp(context->environment_variables[i], "PWD=", 4) == 0)
-			{
-				temp_envp->new_env_key = "PWD";
-				temp_envp->new_env_value = temp_envp->pwd;
-				context->environment_variables[i] = ft_strjoin("PWD=", temp_envp->pwd);
-				ft_add_to_myenvp(context, temp_envp);
-			}
-			else if (ft_strncmp(context->environment_variables[i], "OLDPWD=", 7) == 0)
-			{
-				temp_envp->new_env_key = "OLDPWD";
-				temp_envp->new_env_value = temp_envp->oldpwd;
-				context->environment_variables[i] = ft_strjoin("OLDPWD=", temp_envp->oldpwd);
-				ft_add_to_myenvp(context, temp_envp);
-			}
-			i++;
-		}
-	}
-	i = 0;//Need to fix quotes in input like export my="val=val=val"
-	if (ft_strncmp(command->content, "export", ft_strlen(command->content)) == 0)
-	{
-			while (context->environment_variables[i])
-			{
-				if (ft_strncmp(context->environment_variables[i], temp_envp->new_env_key, ft_strlen(temp_envp->new_env_key)) == 0)
-				{
-					if (temp_envp->new_env_value)
-					{
-						context->environment_variables[i] = ft_strjoin(temp_envp->new_env_key, "=");
-						context->environment_variables[i] = ft_strjoin(context->environment_variables[i], temp_envp->new_env_value);
-						ft_add_to_myenvp(context, temp_envp);
-						return ;
-					}
-				}
-				i++;
-			}
 			if (temp_envp->new_env_value)
 			{
-				temp_key = ft_strjoin(temp_envp->new_env_key, "=");
-				temp_key = ft_strjoin(temp_key, temp_envp->new_env_value);
-				context->environment_variables = ft_copy_envp(context->environment_variables, temp_key);
+				context->environment_variables[i] = ft_strjoin(temp_key, temp_envp->new_env_value);
+				ft_add_to_myenvp(context, temp_envp);
+				return ;
 			}
-			ft_add_to_myenvp(context, temp_envp);
+		}
+		i++;
 	}
+	if (temp_envp->new_env_value)
+	{
+		temp_key = ft_strjoin(temp_key, temp_envp->new_env_value);
+		context->environment_variables = ft_copy_envp(context->environment_variables, temp_key);
+	}
+	free(temp_key);
+	ft_add_to_myenvp(context, temp_envp);
+}
+
+void	ft_update_envp_cd(t_env *temp_envp, t_list *command, t_variable_context *context)
+{
+	int		i;
+
+	i = 0;
+	while (context->environment_variables[i])
+	{
+		if (ft_strncmp(context->environment_variables[i], "PWD=", 4) == 0)
+		{
+			temp_envp->new_env_key = "PWD";
+			temp_envp->new_env_value = temp_envp->pwd;
+			context->environment_variables[i] = ft_strjoin("PWD=", temp_envp->pwd);
+			ft_add_to_myenvp(context, temp_envp);
+		}
+		else if (ft_strncmp(context->environment_variables[i], "OLDPWD=", 7) == 0)
+		{
+			temp_envp->new_env_key = "OLDPWD";
+			temp_envp->new_env_value = temp_envp->oldpwd;
+			context->environment_variables[i] = ft_strjoin("OLDPWD=", temp_envp->oldpwd);
+			ft_add_to_myenvp(context, temp_envp);
+		}
+		i++;
+	}
+}
+
+//To update the entries in envp
+void	ft_update_envp(t_env *temp_envp, t_list *command, t_variable_context *context)
+{
+	if (ft_strncmp(command->content, "cd", ft_strlen(command->content)) == 0)
+		ft_update_envp_cd(temp_envp, command, context);
+	if (ft_strncmp(command->content, "export", ft_strlen(command->content)) == 0)
+		ft_update_envp_exp(temp_envp, command, context);
 }
 
 //To extract variable from envp
@@ -312,56 +368,6 @@ char	**ft_remove_from_local(t_variable_context *context, t_env *temp_envp)
 	return (new);
 }
 
-int	ft_find_equal(char *str)
-{
-	int	i;
-	
-	i = 0;
-	while (str[i] && str[i] != '=')
-		i++;
-	return (i);
-}
-
-char	*ft_get_value(char *str)
-{
-	int		i;
-	char	*result;
-	int		equal_pos;
-	
-	i = 0;
-	equal_pos = ft_find_equal(str);
-	if (ft_strlen(str) - equal_pos == 0)
-		return (NULL);
-	result = ft_calloc(sizeof(char), ft_strlen(str) - equal_pos);
-	equal_pos++;
-	while (equal_pos < ft_strlen(str))
-	{
-		result[i] = str[equal_pos];
-		equal_pos++;
-		i++;
-	}
-	return(result);
-}
-
-char	*ft_get_key(char *str)
-{
-	int		i;
-	char	*result;
-	int		equal_pos;
-	
-	i = 0;
-	equal_pos = ft_find_equal(str);
-	if (equal_pos == 0)
-		return (NULL);
-	result = ft_calloc(sizeof(char), equal_pos + 1);
-	while (i < equal_pos)
-	{
-		result[i] = str[i];
-		i++;
-	}
-	return(result);
-}
-
 void	ft_add_to_others(char *str, t_variable_context *context)
 {
 	t_env	temp_env;
@@ -370,9 +376,8 @@ void	ft_add_to_others(char *str, t_variable_context *context)
 	int		i;
 	
 	i = 0;
-	command = ft_calloc(sizeof(t_list), 3);
+	command = ft_calloc(sizeof(t_list), 1);
 	command->content = "export";
-	command->next = NULL;
 	ft_bzero(&temp_env, sizeof(temp_env));
 	temp_env.new_env_key = ft_get_key(str);
 	copy = ft_strjoin("declare -x ", temp_env.new_env_key);
@@ -380,12 +385,13 @@ void	ft_add_to_others(char *str, t_variable_context *context)
 	while (context->dup_environment_variables && context->dup_environment_variables[i])
 	{
 		if (ft_strncmp(copy, context->dup_environment_variables[i], ft_strlen(copy) + 1) == 0)
-		{
 			ft_update_envp(&temp_env, command, context);
-			ft_add_to_myenvp(context, &temp_env);
-		}	
 		i++;
 	}
+	free(command);
+	free(temp_env.new_env_key);
+	free(copy);
+	free(temp_env.new_env_value);
 }
 
 // t_list	*ft_split_command(t_list *command)
